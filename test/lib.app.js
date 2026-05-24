beb17fe4ae928d7efe9b7777dd5d07cb88204b8d
Mean <61989272+meaningg@users.noreply.github.com>
2026-05-24 16:01:53 +0300

    Fall back to appId for featured app url

--- test/lib.app.js
+++ test/lib.app.js
@@ -1,10 +1,10 @@
-import { assert } from 'chai';
-import validator from 'validator';
-import { assertValidUrl } from './common.js';
-import gplay from '../index.js';
+import { assert } from "chai";
+import validator from "validator";
+import { assertValidUrl } from "./common.js";
+import gplay from "../index.js";
 
 const validateAppDetails = (app) => {
-  assert.equal(app.appId, 'com.sgn.pandapop.gp');
+  assert.equal(app.appId, "com.sgn.pandapop.gp");
   assertValidUrl(app.icon);
 
   assert.isBoolean(app.isAvailableInPlayPass);
@@ -20,13 +20,13 @@ const validateAppDetails = (app) => {
   assert.isString(app.description);
   assert.isString(app.descriptionHTML);
   assert.isString(app.released);
-  assert.equal(app.genreId, 'GAME_PUZZLE');
+  assert.equal(app.genreId, "GAME_PUZZLE");
 
   assert.isArray(app.categories);
   assert.isAbove(app.categories.length, 1);
-  assert.equal(app.categories[0].id, 'GAME_PUZZLE');
-  assert.notEqual(app.categories[1].id, 'GAME_PUZZLE');
-  assert.hasAllKeys(app.categories[0], ['name', 'id']);
+  assert.equal(app.categories[0].id, "GAME_PUZZLE");
+  assert.notEqual(app.categories[1].id, "GAME_PUZZLE");
+  assert.hasAllKeys(app.categories[0], ["name", "id"]);
 
   assert.isString(app.version);
   if (app.size) {
@@ -34,11 +34,11 @@ const validateAppDetails = (app) => {
   }
   assert.isString(app.contentRating);
 
-  assert.equal(app.androidVersion, '7.0');
-  assert.equal(app.androidMaxVersion, 'VARY');
+  assert.equal(app.androidVersion, "7.0");
+  assert.equal(app.androidMaxVersion, "VARY");
 
   assert.isBoolean(app.available);
-  assert.equal(app.priceText, 'Free');
+  assert.equal(app.priceText, "Free");
   assert.equal(app.price, 0);
   assert.isTrue(app.free);
   assert.isTrue(app.offersIAP);
@@ -48,15 +48,18 @@ const validateAppDetails = (app) => {
   assert.isUndefined(app.originalPrice);
   assert.isUndefined(app.discountEndDate);
 
-  assert.equal(app.developer, 'Jam City, Inc.');
-  assert.equal(app.developerId, '5509190841173705883');
-  assert.equal(app.developerInternalID, '5509190841173705883');
+  assert.equal(app.developer, "Jam City, Inc.");
+  assert.equal(app.developerId, "5509190841173705883");
+  assert.equal(app.developerInternalID, "5509190841173705883");
   assertValidUrl(app.developerWebsite);
-  assert(validator.isEmail(app.developerEmail), `${app.developerEmail} is not an email`);
+  assert(
+    validator.isEmail(app.developerEmail),
+    `${app.developerEmail} is not an email`,
+  );
 
   assertValidUrl(app.video);
   assertValidUrl(app.previewVideo);
-  ['1', '2', '3', '4', '5'].map((v) => assert.property(app.histogram, v));
+  ["1", "2", "3", "4", "5"].map((v) => assert.property(app.histogram, v));
 
   assert(app.screenshots.length);
   app.screenshots.map(assertValidUrl);
@@ -70,160 +73,190 @@ const validateAppDetails = (app) => {
   assert.isString(app.recentChanges);
 };
 
-describe('App method', () => {
-  it('should fetch valid application data', () => {
-    return gplay.app({ appId: 'com.sgn.pandapop.gp' })
-      .then((app) => {
-        assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=en&gl=us');
-        assert.equal(app.genre, 'Puzzle');
-        assert.equal(app.androidVersionText, '7.0');
-        validateAppDetails(app);
-      });
+describe("App method", () => {
+  it("should fetch valid application data", () => {
+    return gplay.app({ appId: "com.sgn.pandapop.gp" }).then((app) => {
+      assert.equal(
+        app.url,
+        "https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=en&gl=us",
+      );
+      assert.equal(app.genre, "Puzzle");
+      assert.equal(app.androidVersionText, "7.0");
+      validateAppDetails(app);
+    });
   });
 
-  it('should fetch valid application data for country: es', () => {
-    return gplay.app({
-      appId: 'com.sgn.pandapop.gp',
-      country: 'es',
-      lang: 'es'
-    })
+  it("should fetch valid application data for country: es", () => {
+    return gplay
+      .app({
+        appId: "com.sgn.pandapop.gp",
+        country: "es",
+        lang: "es",
+      })
       .then((app) => {
-        assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=es&gl=es');
-        assert.equal(app.genre, 'Puzles');
-        assert.equal(app.androidVersionText, '7.0');
+        assert.equal(
+          app.url,
+          "https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=es&gl=es",
+        );
+        assert.equal(app.genre, "Puzles");
+        assert.equal(app.androidVersionText, "7.0");
         assert.equal(app.available, true);
         validateAppDetails(app);
       });
   });
 
-  it('should fetch valid application data for country: br', () => {
-    return gplay.app({
-      appId: 'com.sgn.pandapop.gp',
-      country: 'br',
-      lang: 'pt'
-    })
+  it("should fetch valid application data for country: br", () => {
+    return gplay
+      .app({
+        appId: "com.sgn.pandapop.gp",
+        country: "br",
+        lang: "pt",
+      })
       .then((app) => {
-        assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=pt&gl=br');
-        assert.equal(app.genre, 'Quebra-cabeças');
-        assert.equal(app.androidVersionText, '7.0');
+        assert.equal(
+          app.url,
+          "https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=pt&gl=br",
+        );
+        assert.equal(app.genre, "Quebra-cabeças");
+        assert.equal(app.androidVersionText, "7.0");
         assert.equal(app.available, true);
         validateAppDetails(app);
       });
   });
 
   it('should check the developer legal information from the "About the developer" section', () => {
-    return gplay.app({ appId: 'com.soundcloud.android' })
-      .then((app) => {
-        assert.equal(app.developerLegalName, 'SoundCloud Global Limited & Co. KG');
-        assert.equal(app.developerLegalEmail, 'playstore@soundcloud.com');
-        assert.equal(app.developerLegalAddress, 'Rheinsberger Str. 76 /, 10115 Berlin, Germany');
-        assert.equal(app.developerLegalPhoneNumber, '+49 1573 5982119');
-      });
+    return gplay.app({ appId: "com.soundcloud.android" }).then((app) => {
+      assert.equal(
+        app.developerLegalName,
+        "SoundCloud Global Limited & Co. KG",
+      );
+      assert.equal(app.developerLegalEmail, "playstore@soundcloud.com");
+      assert.equal(
+        app.developerLegalAddress,
+        "Karl-Marx-Str. 101, 12043 Berlin, Germany",
+      );
+      assert.equal(app.developerLegalPhoneNumber, "+49 1573 5982119");
+    });
   });
 
-  it('should properly parse a VARY android version', () => {
-    return gplay.app({ appId: 'com.facebook.katana' })
-      .then((app) => {
-        assert.equal(app.androidVersion, 'VARY');
-        assert.equal(app.androidVersionText, 'Varies with device');
-      });
+  it("should properly parse a VARY android version", () => {
+    return gplay.app({ appId: "com.facebook.katana" }).then((app) => {
+      assert.equal(app.androidVersion, "VARY");
+      assert.equal(app.androidVersionText, "Varies with device");
+    });
   });
 
-  it('should get the developer physical address', () => {
-    return gplay.app({ appId: 'com.snapchat.android' })
-      .then((app) => {
-        // Check if developerAddress exists and is a string
-        // The exact address may change over time, so we just verify it exists
-        // Some apps may not have a developer address, so we check if it exists
-        if (app.developerAddress) {
-          assert.isString(app.developerAddress);
-          assert.isTrue(app.developerAddress.length > 0);
-        } else {
-          // If no address is available, that's also valid
-          assert.isUndefined(app.developerAddress);
-        }
-      });
+  it("should get the developer physical address", () => {
+    return gplay.app({ appId: "com.snapchat.android" }).then((app) => {
+      // Check if developerAddress exists and is a string
+      // The exact address may change over time, so we just verify it exists
+      // Some apps may not have a developer address, so we check if it exists
+      if (app.developerAddress) {
+        assert.isString(app.developerAddress);
+        assert.isTrue(app.developerAddress.length > 0);
+      } else {
+        // If no address is available, that's also valid
+        assert.isUndefined(app.developerAddress);
+      }
+    });
   });
 
-  it('should get the privacy policy', () => {
-    return gplay.app({ appId: 'com.snapchat.android' })
-      .then((app) => {
-        assert.equal(app.privacyPolicy, 'http://www.snapchat.com/privacy');
-      });
+  it("should get the privacy policy", () => {
+    return gplay.app({ appId: "com.snapchat.android" }).then((app) => {
+      assert.equal(app.privacyPolicy, "http://www.snapchat.com/privacy");
+    });
   });
 
-  it('should fetch app in spanish', () => {
-    return gplay.app({ appId: 'com.sgn.pandapop.gp', lang: 'es', country: 'ar' })
+  it("should fetch app in spanish", () => {
+    return gplay
+      .app({ appId: "com.sgn.pandapop.gp", lang: "es", country: "ar" })
       .then((app) => {
-        assert.equal(app.appId, 'com.sgn.pandapop.gp');
-        assert.equal(app.title, 'Bubble Shooter: Panda Pop!');
-        assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=es&gl=ar');
+        assert.equal(app.appId, "com.sgn.pandapop.gp");
+        assert.equal(app.title, "Bubble Shooter: Panda Pop!");
+        assert.equal(
+          app.url,
+          "https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=es&gl=ar",
+        );
         assert.isNumber(app.minInstalls);
 
-        assert.equal(app.androidVersion, '7.0');
-        assert.equal(app.androidVersionText, '7.0');
+        assert.equal(app.androidVersion, "7.0");
+        assert.equal(app.androidVersionText, "7.0");
       });
   });
 
-  it('should fetch app in french', () =>
-    gplay.app({ appId: 'com.sgn.pandapop.gp', lang: 'fr', country: 'fr' })
+  it("should fetch app in french", () =>
+    gplay
+      .app({ appId: "com.sgn.pandapop.gp", lang: "fr", country: "fr" })
       .then((app) => {
-        assert.equal(app.appId, 'com.sgn.pandapop.gp');
-        assert.equal(app.title, 'Panda Pop! Jeu de tir à bulles');
-        assert.equal(app.url, 'https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=fr&gl=fr');
+        assert.equal(app.appId, "com.sgn.pandapop.gp");
+        assert.equal(app.title, "Panda Pop! Jeu de tir à bulles");
+        assert.equal(
+          app.url,
+          "https://play.google.com/store/apps/details?id=com.sgn.pandapop.gp&hl=fr&gl=fr",
+        );
         assert.isNumber(app.minInstalls);
 
-        assert.equal(app.androidVersion, '7.0');
-        assert.equal(app.androidVersionText, '7.0');
+        assert.equal(app.androidVersion, "7.0");
+        assert.equal(app.androidVersionText, "7.0");
       }));
 
-  it('should reject the promise for an invalid appId', () =>
-    gplay.app({ appId: 'com.dxco.pandavszombiesasdadad' })
+  it("should reject the promise for an invalid appId", () =>
+    gplay
+      .app({ appId: "com.dxco.pandavszombiesasdadad" })
       .then(() => {
-        throw Error('should not resolve');
+        throw Error("should not resolve");
       })
       .catch((err) => {
-        assert.equal(err.message, 'App not found (404)');
+        assert.equal(err.message, "App not found (404)");
       }));
 
-  it('should reject the promise when appId is not passed', () =>
-    gplay.app({ Testkey: 'com.dxco.pandavszombiesasdadad' })
+  it("should reject the promise when appId is not passed", () =>
+    gplay
+      .app({ Testkey: "com.dxco.pandavszombiesasdadad" })
       .then(() => {
-        throw Error('should not resolve');
+        throw Error("should not resolve");
       })
       .catch((err) => {
-        assert.equal(err.message, 'appId missing');
+        assert.equal(err.message, "appId missing");
       }));
 
-  it('should fetch PriceText for paid apps properly', () => {
-    return gplay.app({ appId: 'com.teslacoilsw.launcher.prime', country: 'in' })
+  it("should fetch PriceText for paid apps properly", () => {
+    return gplay
+      .app({ appId: "com.teslacoilsw.launcher.prime", country: "in" })
       .then((app) => {
-        assert.equal(app.priceText, `₹${app.price.toFixed(2)}`);
-        assert.equal(app.currency, 'INR');
+        assert.isString(app.priceText);
+        assert.include(app.priceText, "₹");
+        // priceText may include locale-specific formatting (e.g., ₹3,250.00)
+        // so compare the raw numeric value after removing non-numeric chars
+        const priceNumeric = app.priceText.replace(/[^\d.]/g, "");
+        assert.equal(priceNumeric, app.price.toFixed(2));
+        assert.equal(app.currency, "INR");
       });
   });
 
-  it('should fetch valid internal developer_id, if it differs from developer_id', () => {
-    return gplay.app({ appId: 'air.com.bitrhymes.bingo' })
-      .then((app) => {
-        assert.equal(app.developerInternalID, '9028773071151690823');
-      });
+  it("should fetch valid internal developer_id, if it differs from developer_id", () => {
+    return gplay.app({ appId: "air.com.bitrhymes.bingo" }).then((app) => {
+      assert.equal(app.developerInternalID, "9028773071151690823");
+    });
   });
 
-  it('should fetch available false for an app is unavailable in country', () => {
-    return gplay.app({ appId: 'com.jlr.landrover.incontrolremote.appstore', country: 'tr' })
+  it("should fetch available false for an app is unavailable in country", () => {
+    return gplay
+      .app({
+        appId: "com.jlr.landrover.incontrolremote.appstore",
+        country: "tr",
+      })
       .then((app) => {
         assert.equal(app.available, false);
       });
   });
 
-  it('should fetch android version limit set for some old apps', () => {
-    return gplay.app({ appId: 'com.facebook.katana' })
-      .then((app) => {
-        // Using Facebook app instead as it's more likely to be available
-        // Just check that android version info is present
-        assert.isString(app.androidVersion);
-        assert.isTrue(app.androidVersion.length > 0);
-      });
+  it("should fetch android version limit set for some old apps", () => {
+    return gplay.app({ appId: "com.facebook.katana" }).then((app) => {
+      // Using Facebook app instead as it's more likely to be available
+      // Just check that android version info is present
+      assert.isString(app.androidVersion);
+      assert.isTrue(app.androidVersion.length > 0);
+    });
   });
 });
--- test/lib.similar.js
+++ test/lib.similar.js
@@ -1,15 +1,25 @@
-import { assert } from 'chai';
-import gplay from '../index.js';
-import { assertValidApp } from './common.js';
+import { assert } from "chai";
+import gplay from "../index.js";
+import { assertValidApp } from "./common.js";
 
-describe('Similar method', () => {
-  it('should fetch a valid application list', () => {
-    return gplay.similar({ appId: 'com.mojang.minecraftpe' })
+describe("Similar method", () => {
+  it("should fetch a valid application list", () => {
+    return gplay
+      .similar({ appId: "com.mojang.minecraftpe" })
       .then((apps) => apps.map(assertValidApp));
   });
 
-  it('should fetch games from different developers', () => {
-    return gplay.similar({ appId: 'com.instagram.android' })
-      .then((apps) => assert.isTrue(apps.some(app => app.developer !== apps[0].developer)));
+  it("should fetch apps from different developers", () => {
+    return gplay
+      .similar({ appId: "com.twitter.android" })
+      .then((apps) =>
+        assert.isTrue(apps.some((app) => app.developer !== apps[0].developer)),
+      );
+  });
+
+  it("should include developer info for each app", () => {
+    return gplay
+      .similar({ appId: "com.mojang.minecraftpe" })
+      .then((apps) => apps.forEach((app) => assert.isString(app.developer)));
   });
 });
